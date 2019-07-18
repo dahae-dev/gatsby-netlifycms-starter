27c591c98a95b58ecd4082f206168daa4d3c2d9f
Kim Dahae <dhkim1014@gmail.com>
2019-07-18 17:39:01 +0900

    Make index page work with netlify cms

---
+++ src/pages/index.md
@@ -0,0 +1,7 @@
+---
+templateKey: index-page
+heading: Hi people
+intro: 
+  heading: Welcome to your new Gatsby site with Netlify CMS.
+  description: Now go build something great.
+---
---
+++ src/templates/index-page.js
@@ -0,0 +1,51 @@
+import React from "react"
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPageTemplate = ({ heading, intro }) => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>{heading}</h1>
+    <p>{intro.heading}</p>
+    <p>{intro.description}</p>
+    <Link to="/blog/">>> Go to blog page</Link>
+  </Layout>
+)
+
+const IndexPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark
+
+  return (
+    <IndexPageTemplate
+      heading={frontmatter.heading}
+      intro={frontmatter.intro}
+    />
+  )
+}
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
+}
+
+export default IndexPage
+
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        heading
+        intro {
+          heading
+          description
+        }
+      }
+    }
+  }
+`
