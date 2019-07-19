5cd2a15ee5b9faac13e3d578a7d2437b0c46ad83
Kim Dahae <dhkim1014@gmail.com>
2019-07-19 11:24:50 +0900

    Export IndexPageTemplate to access from cms.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -5,7 +5,7 @@ import { Link, graphql } from 'gatsby'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPageTemplate = ({ heading, intro }) => (
+export const IndexPageTemplate = ({ heading, intro }) => (
   <Layout>
     <SEO title="Home" />
     <h1>{heading}</h1>
