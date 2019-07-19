183646caf5b9c5c86e546c038d89b782c20c3c7c
Kim Dahae <dhkim1014@gmail.com>
2019-07-19 10:52:17 +0900

    Enable cms preview

---
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { IndexPageTemplate } from '../../templates/index-page'
+
+const IndexPagePreview = ({ entry }) => {
+  const data = entry.getIn(['data']).toJS()
+
+  if (data) {
+    return (
+      <IndexPageTemplate   
+        heading={data.heading}
+        intro={data.intro}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
+}
+
+IndexPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  })
+}
+
+export default IndexPagePreview
