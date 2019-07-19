54eccad59bf311e98a1044afbd13e795e8fa42d0
Kim Dahae <dhkim1014@gmail.com>
2019-07-19 17:53:30 +0900

    Enable markdown content to show in cms

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -3,19 +3,16 @@ import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
 
 const IndexPagePreview = ({ entry, widgetFor }) => {
-  console.log("entry: ", entry)
   const data = entry.getIn(['data']).toJS()
-  console.log("data: ", data)
-  console.log("body: ", widgetFor('body'))
 
   if (data) {
     return (
       <IndexPageTemplate   
-        title={entry.getIn(['data', 'title'])}
-        heading={entry.getIn(['data', 'intro', 'heading'])}
-        description={entry.getIn(['data', 'intro', 'description'])}
-        src={entry.getIn(['data', 'image', 'src'])}
-        alt={entry.getIn(['data', 'image', 'alt'])}
+        title={data.title}
+        heading={data.intro.heading}
+        description={data.intro.description}
+        src={data.image.src}
+        alt={data.image.alt}
         content={widgetFor('body')}
       />
     )
